f77b199e42682c78bb2a95b473fac54ddbb513e2
Mike Dacre <mike@dacre.me>
2022-12-12 02:27:03 -0800

    Added taxonomy pages, fixed breadcrumbs, added file indexes
    New taxonomy templates for /tags and /categories
    
    Fixed bug that broke php breadcrumb page
    
    Added file section pages for /resources

--- layouts/_default/list.php
+++ layouts/_default/list.php
@@ -1,10 +1,28 @@
 {{ define "main" }}
-{{- partial "pageHeader.html" . -}}
+{{ partial "pageHeader.html" . }}
 
+<br>
 <section id="page-section" class="section">
   <div class="container">
     {{ .Content }}
   </div>
 </section>
+
+{{ if .Pages }}
+<section id="page-section" class="section">
+  <div class="container">
+    <h2>Subsections and Pages</h2>
+    {{ range .Pages.ByPublishDate.Reverse }}
+      <p>
+        <h3><a class="title" href="{{ .RelPermalink }}">{{ .Title }}</a></h3>
+        {{ partial "metadata.html" . }}
+        <a class="summary" href="{{ .RelPermalink }}">
+            <p>{{ .Summary }}</p>
+        </a>
+      </p>
+    {{ end }}
+  </div>
+</section>
+{{ end }}
 
 {{ end }}
--- layouts/learning/list.php
+++ layouts/learning/list.php
@@ -1,9 +1,11 @@
 {{ define "main" }}
 {{ partial "pageHeader.html" . }}
 
+<br>
 <section id="page-section" class="section">
   <div class="container">
     {{ .Content }}
   </div>
 </section>
+
 {{ end }}
